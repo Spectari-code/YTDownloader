66edef50bd22c2cbc8d4c5f38d8b1c18481e49ed
Spectari <54914548+Spectari-code@users.noreply.github.com>
2021-01-04 16:42:10 +0200

    Add project files.

---
+++ YTDownloader/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using VideoLibrary;
+
+namespace YTDownloader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var youTube = YouTube.Default; // starting point for Youtube actions.
+            
+            Console.WriteLine("Youtube Downloader\nEnter the youtube link: ");
+            var id = Console.ReadLine(); //id is the link
+            Console.WriteLine("Downloading...");
+
+            var video = youTube.GetVideo(id); //download the vid
+            // Add Download path.
+            File.WriteAllBytes(@"C:\Users\Bligh\Downloads\" + video.FullName, video.GetBytes());
+            Console.WriteLine("Finished.");
+            
+            Console.ReadKey();
+            Console.WriteLine("Closing...");
+        }
+    }
+}
